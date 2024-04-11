6cd0d9ead0d888b258f1661ade0addc51fca554e
Gautam-666 <2022.gautam.rai@ves.ac.in>
2024-04-11 18:40:54 +0530

    Os project changes

--- src/OS_PRACTICAL/DynamicPartition.java
+++ src/OS_PRACTICAL/DynamicPartition.java
@@ -3,59 +3,71 @@ package OS_PRACTICAL;
 import java.util.*;
 
 public class DynamicPartition {
-    public static void Bestfit(int[] partitions,int process){
-        int index = -1;
-        int largestpartition = partitions[0];
-        for (int i = 1; i < partitions.length; i++) {
-            if(largestpartition >= process && partitions[i] >= process) {
-                largestpartition = Math.min(partitions[i],largestpartition);
+    public static int[] bestFit(int[] partitions, int[] processes) {
+        int[] allocation = new int[processes.length];
+        Arrays.fill(allocation, -1);
+
+        for (int i = 0; i < processes.length; i++) {
+            int bestIndex = -1;
+            for (int j = 0; j < partitions.length; j++) {
+                if (partitions[j] >= processes[i]) {
+                    if (bestIndex == -1 || partitions[j] < partitions[bestIndex]) {
+                        bestIndex = j;
+                    }
+                }
+            }
+            if (bestIndex != -1) {
+                allocation[i] = bestIndex;
+                partitions[bestIndex] -= processes[i];
             }
         }
+        return allocation;
+    }
 
-        for (int i = 0; i < partitions.length; i++) {
-            if(largestpartition == partitions[i]){
-                index = i+1;
-                break;
+    public static int[] firstFit(int[] partitions, int[] processes) {
+        int[] allocation = new int[processes.length];
+        Arrays.fill(allocation, -1);
+
+        for (int i = 0; i < processes.length; i++) {
+            for (int j = 0; j < partitions.length; j++) {
+                if (partitions[j] >= processes[i]) {
+                    allocation[i] = j;
+                    partitions[j] -= processes[i];
+                    break;
+                }
             }
         }
+        return allocation;
+    }
 
-        System.out.println("Element stored in Best fit at index: " + index);
+    public static int[] worstFit(int[] partitions, int[] processes) {
+        int[] allocation = new int[processes.length];
+        Arrays.fill(allocation, -1);
+
+        for (int i = 0; i < processes.length; i++) {
+            int worstIndex = -1;
+            for (int j = 0; j < partitions.length; j++) {
+                if (partitions[j] >= processes[i]) {
+                    if (worstIndex == -1 || partitions[j] > partitions[worstIndex]) {
+                        worstIndex = j;
+                    }
+                }
+            }
+            if (worstIndex != -1) {
+                allocation[i] = worstIndex;
+                partitions[worstIndex] -= processes[i];
+            }
+        }
+        return allocation;
     }
 
     public static void main(String[] args) {
-        int[] partitions = {200,100,110,800,265,154};
-        int process = 110;
-        Bestfit(partitions,process);
-        WorstFit(partitions,process);
-        FirstFit(partitions,process);
-    }
-
-    private static void FirstFit(int[] partitions, int process) {
-        int index = -1;
-        for (int i = 0; i < partitions.length; i++) {
-            if(process <= partitions[i]){
-                index = i+1;
-                break;
-            }
-        }
-        System.out.println("Element stored in first fit at index: " + index);
-    }
-
-    private static void WorstFit(int[] partitions, int process) {
-        int index = -1;
-        int largestpartition = partitions[0];
-        for (int i = 1; i < partitions.length; i++) {
-            if(largestpartition > process)
-                largestpartition = Math.max(partitions[i],largestpartition);
-        }
-
-        for (int i = 0; i < partitions.length; i++) {
-            if(largestpartition == partitions[i]){
-                index = i+1;
-                break;
-            }
-        }
-
-        System.out.println("Element stored in worst fit at index: " + index);
+        int[] partitions = {100, 200, 212, 300, 400, 500};
+        int[] processes = {212, 417, 112, 426};
+        System.out.println("Partitions: " + Arrays.toString(partitions));
+        System.out.println("Processes: " + Arrays.toString(processes));
+        System.out.println("\nBest Fit Allocation: " + Arrays.toString(bestFit(partitions.clone(), processes)));
+        System.out.println("First Fit Allocation: " + Arrays.toString(firstFit(partitions.clone(), processes)));
+        System.out.println("Worst Fit Allocation: " + Arrays.toString(worstFit(partitions.clone(), processes)));
     }
 }
